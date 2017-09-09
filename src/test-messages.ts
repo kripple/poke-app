8c8668ff70aee386794fc7825222a545aab53269
Kelly Ripple <kelly.m.ripple@gmail.com>
2017-09-09 12:43:55 -0400

    move test messages to file

---
+++ src/test-messages.ts
@@ -0,0 +1,21 @@
+export let BlisseyMessage = `blissey (336681882487881739)
+Blissey: <@&290895644917497856>
+
+Despawns @ 08:30:15am __**(3m 7s)**__
+Level: 20 - IV: 12/3/2
+- **Zen Headbutt** (DPS: 10.91)
+- **Psychic** (DPS: 35.71)
+
+**PGAN Map**: https://co-frontrange.pogoalerts.net/?lat=40.3853566197&lon=-105.039349113&zoom=16
+**Google Map**: http://maps.google.com/maps?q=40.3853566197,-105.0393491128`;
+
+export let IvRaresMessage = `100iv_rares (263032521325936642)
+Magikarp: <@&290895696851369984>
+
+Despawns @ 10:45:47am __**(11m 17s)**__
+Level: 9 - IV: 15/15/15
+- **Splash** (DPS: 0.0)
+- **Struggle** (DPS: 15.91)
+
+**PGAN Map**: https://md-moco.pogoalerts.net/?lat=39.0766976227&lon=-77.1064013076&zoom=16
+**Google Map**: http://maps.google.com/maps?q=39.0766976227,-77.1064013076`;
