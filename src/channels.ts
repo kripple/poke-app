d09d36289a55a70128d0c95392b63343ef42b6a8
Kelly Ripple <kelly.m.ripple@gmail.com>
2017-09-09 12:42:43 -0400

    add a channel

---
+++ src/channels.ts
@@ -0,0 +1,8 @@
+// TBD: store channels in dynamo, make interface to add/remove channels
+export const Channels = {
+  'lvl-4-raids': 340247989169815552,
+  'level-5-raikou-herndon': 352802959320416258,
+  'level-5-raikou-reston': 352924882947801109,
+  '100iv_rares': 263032521325936642,
+  '100iv_commons': 282403352199954432
+};
